ac8a5f0b4aabbc01115af6f9ce67e321095c57dd
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-04-11 20:18:32 -0300

    blbablabla

---
+++ script.js
@@ -0,0 +1,80 @@
+//alert("CUIDADOOOO!!!!!!");
+
+
+console.log("Externo");
+
+const PI = 3.14;
+console.log(PI);
+
+var nome = "joãozinho";
+console.log(nome);
+
+let sobrenome = "mario";
+console.log(sobrenome);
+
+let n1 = 2;
+let n2 = 3;
+console.log(n1 + n2);
+
+n2 = "textinho";
+console.log(n2);
+
+let bool = true;
+console.log(bool);
+bool = false;
+console.log(bool);
+
+//undefined
+let sem_definir;
+console.log(sem_definir);
+
+//null
+let vnula = null;
+console.log(sem_definir == vnula);
+console.log("" == vnula);
+
+//string
+let string = "Texto";
+let string2 = String("texto");
+
+//Potenciação X
+let x = 2;
+console.log(x * x);
+console.log(x ** 2);
+
+//resto
+let a = 3;
+let b = 2;
+
+console.log(a / b);
+console.log(a % b);
+
+//operadores
+let var01 = true;
+let var02 = true;1
+
+//and
+console.log(var01 && var02);
+
+//or
+console.log(var01 || var02);
+
+//not
+console.log(var01);
+
+
+//comparação
+console.log(bool === "0");
+
+// diferença
+console.log(10 != 9);
+
+//maior
+console.log(10 > 3);
+
+//menor
+console.log(10 < 3);
+
+//comparação string
+console.log("A" > "B");
+console.log("B" > "A");
